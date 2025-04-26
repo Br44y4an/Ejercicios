b87a39aac84eee54875d95a88aa60b88e667ea12
Br44y4an <162704565+Br44y4an@users.noreply.github.com>
2025-04-25 21:57:20 -0500

    exercice_colors

--- practicas-estados/src/App.tsx
+++ practicas-estados/src/App.tsx
@@ -1,16 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-const colors = [
-  "red",
-  "blue",
-  "green",
-  "yellow",
-  "purple",
-  "orange",
-  "pink",
-  "brown",
-];
+const colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown"];
 
 function App() {
   // estados
@@ -18,13 +9,13 @@ function App() {
 
   // funciones
   const changeColor = () => {
-    
+    setColor(colors[Math.floor(Math.random() * 7)]);
   };
 
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 10 }}>
-      <div style={{backgroundColor: color, width: 50, aspectRatio: 1}}/>
-      <button>Cambiar a color aleatorio</button>
+      <div style={{ backgroundColor: color, width: 50, aspectRatio: 1 }} />
+      <button onClick={changeColor}>Cambiar a color aleatorio</button>
     </div>
   );
 }
